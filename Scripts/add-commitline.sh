57fa3d3ef357a28f4775bdc79a0b7760158f7bc2
yuler <joyful4yu@gmail.com>
2020-01-12 21:38:57 +0800

    TMP: add-commitlint.sh

---
+++ Scripts/add-commitline.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+npm install --save-dev @commitlint/{cli,config-conventional,prompt-cli}
+
+# "scripts": {
+#  "commit": "commit"
+# }
+
+# Add git hook
+# "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
