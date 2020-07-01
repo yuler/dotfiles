95423463811e8477ff12405f0cd4e8cfc9d95ba6
yuler <joyful4yu@gmail.com>
2020-07-01 17:11:32 +0800

    Format code style

--- vpn/install.ubuntu.18.04.sh
+++ vpn/install.ubuntu.18.04.sh
@@ -10,7 +10,7 @@ COLOR_ERROR="\e[38;5;198m"
 COLOR_NONE="\e[0m"
 COLOR_SUCC="\e[92m"
 
-update_core(){
+update_core() {
     echo -e "${COLOR_ERROR}当前系统内核版本太低 <$VERSION_CURR>,需要更新系统内核.${COLOR_NONE}"
     sudo apt install -y -qq --install-recommends linux-generic-hwe-18.04
     sudo apt autoremove
@@ -19,18 +19,18 @@ update_core(){
     sudo reboot
 }
 
-check_bbr(){
+check_bbr() {
     has_bbr=$(lsmod | grep bbr)
 
     # 如果已经发现 bbr 进程
-    if [ -n "$has_bbr" ] ;then
+    if [ -n "$has_bbr" ]; then
         echo -e "${COLOR_SUCC}TCP BBR 拥塞控制算法已经启动${COLOR_NONE}"
     else
         start_bbr
     fi
 }
 
-start_bbr(){
+start_bbr() {
     echo "启动 TCP BBR 拥塞控制算法"
     sudo modprobe tcp_bbr
     echo "tcp_bbr" | sudo tee --append /etc/modules-load.d/modules.conf
@@ -43,7 +43,7 @@ start_bbr(){
 
 install_bbr() {
     # 如果内核版本号满足最小要求
-    if [ $VERSION_CURR > $VERSION_MIN ]; then
+    if [ $VERSION_CURR ] >$VERSION_MIN; then
         check_bbr
     else
         update_core
@@ -65,12 +65,11 @@ install_docker() {
     fi
 }
 
-
-check_container(){
+check_container() {
     has_container=$(sudo docker ps --format "{{.Names}}" | grep "$1")
 
     # test 命令规范： 0 为 true, 1 为 false, >1 为 error
-    if [ -n "$has_container" ] ;then
+    if [ -n "$has_container" ]; then
         return 0
     else
         return 1
@@ -96,7 +95,7 @@ create_cert() {
     echo -e "${COLOR_ERROR}注意：生成证书前,需要将域名指向一个有效的 IP,否则无法创建证书.${COLOR_NONE}"
     read -p "是否已经将域名指向了 IP？[Y/n]" has_record
 
-    if ! [[ "$has_record" = "Y" ]] ;then
+    if ! [[ "$has_record" = "Y" ]]; then
         echo "请操作完成后再继续."
         return
     fi
@@ -108,11 +107,11 @@ create_cert() {
 
 install_gost() {
     if ! [ -x "$(command -v docker)" ]; then
-        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}" 
+        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}"
         return
     fi
 
-    if check_container gost ; then
+    if check_container gost; then
         echo -e "${COLOR_ERROR}Gost 容器已经在运行了，你可以手动停止容器，并删除容器，然后再执行本命令来重新安装 Gost。 ${COLOR_NONE}"
         return
     fi
@@ -121,9 +120,9 @@ install_gost() {
     read -p "请输入你要使用的域名：" DOMAIN
     read -p "请输入你要使用的用户名:" USER
     read -p "请输入你要使用的密码:" PASS
-    read -p "请输入HTTP/2需要侦听的端口号(443)：" PORT 
+    read -p "请输入HTTP/2需要侦听的端口号(443)：" PORT
 
-    if [[ -z "${PORT// }" ]] || ! [[ "${PORT}" =~ ^[0-9]+$ ]] || ! [ "$PORT" -ge 1 -a "$PORT" -le 655535 ]; then
+    if [[ -z "${PORT// /}" ]] || ! [[ "${PORT}" =~ ^[0-9]+$ ]] || ! [ "$PORT" -ge 1 -a "$PORT" -le 655535 ]; then
         echo -e "${COLOR_ERROR}非法端口,使用默认端口 443 !${COLOR_NONE}"
         PORT=443
     fi
@@ -143,57 +142,57 @@ crontab_exists() {
     crontab -l 2>/dev/null | grep "$1" >/dev/null 2>/dev/null
 }
 
-create_cron_job(){
+create_cron_job() {
     # 写入前先检查，避免重复任务。
     if ! crontab_exists "certbot renew --force-renewal"; then
-        echo "0 0 1 * * /usr/bin/certbot renew --force-renewal" >> /var/spool/cron/crontabs/root
+        echo "0 0 1 * * /usr/bin/certbot renew --force-renewal" >>/var/spool/cron/crontabs/root
         echo "${COLOR_SUCC}成功安装证书renew定时作业！${COLOR_NONE}"
     else
         echo "${COLOR_SUCC}证书renew定时作业已经安装过！${COLOR_NONE}"
     fi
 
-    if ! crontab_exists "docker restart gost"; then 
-        echo "5 0 1 * * /usr/bin/docker restart gost" >> /var/spool/cron/crontabs/root
+    if ! crontab_exists "docker restart gost"; then
+        echo "5 0 1 * * /usr/bin/docker restart gost" >>/var/spool/cron/crontabs/root
         echo "${COLOR_SUCC}成功安装gost更新证书定时作业！${COLOR_NONE}"
     else
         echo "${COLOR_SUCC}gost更新证书定时作业已经成功安装过！${COLOR_NONE}"
     fi
 }
 
-install_shadowsocks(){
+install_shadowsocks() {
     if ! [ -x "$(command -v docker)" ]; then
-        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}" 
+        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}"
         return
     fi
 
-    if check_container ss ; then
+    if check_container ss; then
         echo -e "${COLOR_ERROR}ShadowSocks 容器已经在运行了，你可以手动停止容器，并删除容器，然后再执行本命令来重新安装 ShadowSocks。${COLOR_NONE}"
         return
     fi
 
     echo "准备启动 ShadowSocks 代理程序,为了安全,需要使用用户名与密码进行认证."
     read -p "请输入你要使用的密码:" PASS
-    read -p "请输入ShadowSocks需要侦听的端口号(1984)：" PORT 
+    read -p "请输入ShadowSocks需要侦听的端口号(1984)：" PORT
 
     BIND_IP=0.0.0.0
 
-    if [[ -z "${PORT// }" ]] || ! [[ "${PORT}" =~ ^[0-9]+$ ]] || ! [ "$PORT" -ge 1 -a "$PORT" -le 655535 ]; then
+    if [[ -z "${PORT// /}" ]] || ! [[ "${PORT}" =~ ^[0-9]+$ ]] || ! [ "$PORT" -ge 1 -a "$PORT" -le 655535 ]; then
         echo -e "${COLOR_ERROR}非法端口,使用默认端口 1984 !${COLOR_NONE}"
         PORT=1984
-    fi 
+    fi
 
     sudo docker run -dt --name ss \
         -p ${PORT}:${PORT} mritd/shadowsocks \
         -s "-s ${BIND_IP} -p ${PORT} -m aes-256-cfb -k ${PASS} --fast-open"
 }
 
-install_vpn(){
+install_vpn() {
     if ! [ -x "$(command -v docker)" ]; then
-        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}" 
+        echo -e "${COLOR_ERROR}未发现Docker，请求安装 Docker ! ${COLOR_NONE}"
         return
     fi
 
-    if check_container vpn ; then
+    if check_container vpn; then
         echo -e "${COLOR_ERROR}VPN 容器已经在运行了，你可以手动停止容器，并删除容器，然后再执行本命令来重新安装 VPN。${COLOR_NONE}"
         return
     fi
@@ -209,72 +208,71 @@ install_vpn(){
         -p 500:500/udp \
         -p 4500:4500/udp \
         -p 1701:1701/tcp \
-        -p 1194:1194/udp  \
+        -p 1194:1194/udp \
         siomiz/softethervpn
 }
 
-install_brook(){
+install_brook() {
     brook_file="/usr/local/brook/brook"
     [[ -e ${brook_file} ]] && echo -e "${COLOR_ERROR}Brook 已经安装，请检查!" && return
-    wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/brook.sh &&\
+    wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/brook.sh &&
         chmod +x brook.sh && sudo bash brook.sh
 }
 
 # TODO: install v2ray
 
-init(){
+init() {
     VERSION_CURR=$(uname -r | awk -F '-' '{print $1}')
     VERSION_MIN="4.9.0"
 
-    OIFS=$IFS  # Save the current IFS (Internal Field Separator)
-    IFS=','    # New IFS
+    OIFS=$IFS # Save the current IFS (Internal Field Separator)
+    IFS=','   # New IFS
 
     COLUMNS=50
     echo -e "\n菜单选项\n"
 
-    while [ 1 == 1 ]
-    do
+    while [ 1 == 1 ]; do
         PS3="Please select a option:"
         re='^[0-9]+$'
         select opt in "安装 TCP BBR 拥塞控制算法" \
-                    "安装 Docker 服务程序" \
-                    "创建 SSL 证书" \
-                    "安装 Gost HTTP/2 代理服务" \
-                    "安装 ShadowSocks 代理服务" \
-                    "安装 VPN/L2TP 服务" \
-                    "安装 Brook 代理服务" \
-                    "创建证书更新 CronJob" \
-                    "退出" ; do
+            "安装 Docker 服务程序" \
+            "创建 SSL 证书" \
+            "安装 Gost HTTP/2 代理服务" \
+            "安装 ShadowSocks 代理服务" \
+            "安装 VPN/L2TP 服务" \
+            "安装 Brook 代理服务" \
+            "创建证书更新 CronJob" \
+            "退出"; do
 
-            if ! [[ $REPLY =~ $re ]] ; then
+            if ! [[ $REPLY =~ $re ]]; then
                 echo -e "${COLOR_ERROR}Invalid option. Please input a number.${COLOR_NONE}"
-                break;
-            elif (( REPLY == 1 )) ; then
+                break
+            elif ((REPLY == 1)); then
                 install_bbr
-                break;
-            elif (( REPLY == 2 )) ; then
+                break
+            elif ((REPLY == 2)); then
                 install_docker
-                break 
-            elif (( REPLY == 3 )) ; then
+                break
+            elif ((REPLY == 3)); then
                 create_cert
                 loop=1
                 break
-            elif (( REPLY == 4 )) ; then
+            elif ((REPLY == 4)); then
                 install_gost
                 break
-            elif (( REPLY == 5  )) ; then
+            elif ((REPLY == 5)); then
                 install_shadowsocks
                 break
-            elif (( REPLY == 6 )) ; then
+            elif ((REPLY == 6)); then
                 install_vpn
                 break
-            elif (( REPLY == 7 )) ; then
+            elif ((REPLY == 7)); then
                 install_brook
                 break
-            elif (( REPLY == 8 )) ; then
+            elif ((REPLY == 8)); then
                 create_cron_job
                 break
-            elif (( REPLY == 9 )) ; then
+            elif ((REPLY == 9)); then
                 exit
             else
                 echo -e "${COLOR_ERROR}Invalid option. Try another one.${COLOR_NONE}"
@@ -282,7 +280,7 @@ init(){
         done
     done
 
-     IFS=$OIFS  # Restore the IFS
+    IFS=$OIFS # Restore the IFS
 }
 
 init
