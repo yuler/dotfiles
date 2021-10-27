0e95df3cf1ccd61019ba04e8e8bca59879269038
yuler <joyful4yu@gmail.com>
2021-10-27 11:18:14 +0800

    Add `.gitconfig` file

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 cat <<EOF
-Link .aliases & .functions to ~
+Link .aliases & .functions & .gitconfig to ~
 EOF
 
 ln -s $PWD/.aliases $HOME/.aliases
 ln -s $PWD/.functions $HOME/.functions
+ln -s $PWD/.gitconfig $HOME/.gitconfig
+cp $PWD/.gitconfig.includes $HOME/.gitconfig.includes
