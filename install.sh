20d1b52e5e2f2291245d1d12904032df300a90db
yuler <is.yuler@gmail.com>
2021-12-20 14:19:28 +0800

    Adjust install.sh scritpt & gitconfig set

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@ cat <<EOF
 Link .{path,bash_prompt,exports,aliases,functions,extra,gitconfig} to ~
 EOF
 
+touch .gitconfig.includes
+touch .gitconfig.local
+touch .exports.local
+
 for file in .{path,bash_prompt,exports,exports.local,aliases,functions,extra,gitconfig,gitconfig.local}; do
     [ -r "$file" ] && [ -f "$file" ] && ln -s $PWD/$file $HOME/$file
 done
