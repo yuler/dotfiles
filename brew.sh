7698020148055c30bad96e6149aa7ad262176c73
yuler <is.yuler@gmail.com>
2021-12-20 14:45:00 +0800

    Add `brew.sh`

---
+++ brew.sh
@@ -0,0 +1,19 @@
+# WIP
+# Install Homebrew & apps
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
+brew update
+brew upgrade
+
+apps=(
+   gh
+   go
+   glab
+)
+
+brew install "${apps[@]}"
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
