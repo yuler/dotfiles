03d5b6b4429b39bd735567e0eb78ff068c6b94c8
yuler <is.yuler@gmail.com>
2021-12-20 19:30:45 +0800

    Fix `install.sh` script add `.gitconfig.includes`

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ touch .gitconfig.includes
 touch .gitconfig.local
 touch .exports.local
 
-for file in .{path,bash_prompt,exports,exports.local,aliases,functions,extra,gitconfig,gitconfig.local}; do
+for file in .{path,bash_prompt,exports,exports.local,aliases,functions,extra,gitconfig,gitconfig.includes,gitconfig.local}; do
     [ -r "$file" ] && [ -f "$file" ] && ln -s $PWD/$file $HOME/$file
 done
 
